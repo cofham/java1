2256f6ffa61bd03fabaad14c03366dd7115cd12f
jns64 <jns64@DESKTOP-GOUURKB>
2021-05-28 16:44:28 +0900

    Java1

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,13 @@
+
+public class StringApp {
+	public static void main(String[] args) {
+		//Character VS String
+		System.out.println("Hello World");	//String
+		System.out.println('H');	//Character
+		System.out.println("H");
+		
+		System.out.println("Hello \nWorld");	//new line
+		System.out.println("Hello \"World\"");	//Hello "World"
+		
+	}
+}
